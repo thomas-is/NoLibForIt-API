911a001d53d4c5c109d1b9c7ec6719e0c53c5ab4
thomas-is <thomas@0l.fi>
2021-08-05 09:01:45 +0200

    :elephant: add generic content method

--- Answer.php
+++ Answer.php
@@ -5,26 +5,28 @@ namespace NoLibForIt\API;
 class Answer {
 
   private static function http( int $code ) {
-    $protocol = @$_SERVER["SERVER_PROTOCOL"] ?
-      $_SERVER["SERVER_PROTOCOL"] :
-      "HTTP/1.1";
+    $protocol = @$_SERVER['SERVER_PROTOCOL'] ?: "HTTP/1.1";
     header("$protocol $code");
-  }
-
-  public static function json( $code, $obj ) {
-    $body = json_encode($obj, JSON_INVALID_UTF8_IGNORE);
-    if( empty($body) ) {
-      self::code(520);
-    } else {
-      self::http($code);
-      header("Content-Type: application/json");
-      die($body);
-    }
   }
 
   public static function code( int $code ) {
     self::http($code);
     die();
+  }
+
+  public static function content( int $code, $type, $body ) {
+      self::http($code);
+      header("Content-Type: $type");
+      die($body);
+  }
+
+  public static function json( int $code, $obj ) {
+    $body = json_encode($obj, JSON_INVALID_UTF8_IGNORE);
+    if( empty($body) ) {
+      self::code(520);
+    } else {
+      self::content($code,"application/json",$body);
+    }
   }
 
 }
